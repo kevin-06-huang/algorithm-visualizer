23caa7191e6e5cac879dd66df946d09b62c8ea6e
Kevin <kevin.06.huang@gmail.com>
2023-08-24 13:43:59 -0700

    Separate out logics

--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,16 @@
-// Sample data
-const data = [{ x: 0, y: 0 }];
+const data = require("./data");
+const { drawPoints, initialize, width, height } = require("./visualization");
 
-// Set up dimensions
-const width = 800;
-const height = 800;
+const svg = initialize();
 
-// Create SVG container
-const svg = d3.select("#xy-graph").attr("width", width).attr("height", height);
-
-// Create scales
-const xScale = d3.scaleLinear().domain([-20, 20]).range([0, width]);
-
-const yScale = d3.scaleLinear().domain([-20, 20]).range([height, 0]);
-
-const xAxisGroup = svg
-  .append("g")
-  .attr("class", "x-axis")
-  .attr("transform", `translate(0, ${height / 2})`);
-
-const yAxisGroup = svg
-  .append("g")
-  .attr("class", "y-axis")
-  .attr("transform", `translate(${width / 2}, 0)`);
-
-const xAxis = d3.axisBottom(xScale);
-const yAxis = d3.axisLeft(yScale);
-
-xAxisGroup.call(xAxis);
-yAxisGroup.call(yAxis);
-
-// Create circles based on data
-function drawPoints() {
-  const circles = svg.selectAll("circle").data(data);
-
-  // Handle the enter selection - for new data points
-  circles
-    .enter()
-    .append("circle")
-    .attr("cx", (d) => xScale(d.x))
-    .attr("cy", (d) => yScale(d.y))
-    .attr("r", 5);
-
-  // Handle the update selection - for existing data points
-  circles
-    .attr("cx", (d) => xScale(d.x))
-    .attr("cy", (d) => yScale(d.y))
-    .attr("r", 5);
-
-  // Handle the exit selection - for data points that no longer exist
-  circles.exit().remove();
-}
-
-drawPoints();
+drawPoints(svg);
 
 svg.on("click", function () {
   // Get mouse positions relative to the SVG container
   const [x, y] = d3.mouse(this);
-  const scaledX = (x - 400) / 20;
-  const scaledY = -(y - 400) / 20;
+  const scaledX = (x - width / 2) / 20;
+  const scaledY = -(y - height / 2) / 20;
 
   data.push({ x: scaledX, y: scaledY });
-  drawPoints();
+  drawPoints(svg);
 });
---
+++ src/js/visualization.js
@@ -0,0 +1,61 @@
+const data = require("./data");
+
+// Set up dimensions
+const width = 800;
+const height = 800;
+
+let xScale, yScale;
+
+function initialize() {
+  // Create SVG container
+  const svg = d3
+    .select("#xy-graph")
+    .attr("width", width)
+    .attr("height", height);
+
+  // Create scales
+  xScale = d3.scaleLinear().domain([-20, 20]).range([0, width]);
+
+  yScale = d3.scaleLinear().domain([-20, 20]).range([height, 0]);
+
+  const xAxisGroup = svg
+    .append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0, ${height / 2})`);
+
+  const yAxisGroup = svg
+    .append("g")
+    .attr("class", "y-axis")
+    .attr("transform", `translate(${width / 2}, 0)`);
+
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+  return svg;
+}
+
+// Create circles based on data
+function drawPoints(svg) {
+  const circles = svg.selectAll("circle").data(data);
+
+  // Handle the enter selection - for new data points
+  circles
+    .enter()
+    .append("circle")
+    .attr("cx", (d) => xScale(d.x))
+    .attr("cy", (d) => yScale(d.y))
+    .attr("r", 5);
+
+  // Handle the update selection - for existing data points
+  circles
+    .attr("cx", (d) => xScale(d.x))
+    .attr("cy", (d) => yScale(d.y))
+    .attr("r", 5);
+
+  // Handle the exit selection - for data points that no longer exist
+  circles.exit().remove();
+}
+
+module.exports = { drawPoints, initialize, width, height };
