15b71edf9d766d24b9f292409ce7b9243ca1e603
Kevin <kevin.06.huang@gmail.com>
2023-08-23 20:18:32 -0700

    Remove react

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-
-const App = () => {
-  return <></>;
-};
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
