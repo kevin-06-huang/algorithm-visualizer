9c6251903f2631bbd8d8d045a68479b67c3b179f
Kevin <kevin.06.huang@gmail.com>
2023-08-24 17:11:55 -0700

    Switch to a version that carries the path internally

--- src/ts/algorithms.ts
+++ src/ts/algorithms.ts
@@ -2,55 +2,54 @@ function distance(p1: [number, number], p2: [number, number]): number {
   return Math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2);
 }
 
-function heldKarp(dists: number[][]): [number, number[][]] {
+function heldKarp(dists: number[][]): [number, number[]] {
   const n: number = dists.length;
   const dp: number[][] = Array.from(Array(1 << n), () => Array(n).fill(Infinity));
+  const prev: number[][] = Array.from(Array(1 << n), () => Array(n).fill(-1));
+  
   dp[1][0] = 0;
 
-  console.log(dp);
-  
   for (let mask = 1; mask < (1 << n); mask++) {
       for (let u = 0; u < n; u++) {
           if (!(mask & (1 << u))) continue;
           for (let v = 0; v < n; v++) {
               if (mask & (1 << v) && u != v) {
-                  dp[mask][u] = Math.min(dp[mask][u], dp[mask ^ (1 << u)][v] + dists[v][u]);
+                  if (dp[mask][u] > dp[mask ^ (1 << u)][v] + dists[v][u]) {
+                      dp[mask][u] = dp[mask ^ (1 << u)][v] + dists[v][u];
+                      prev[mask][u] = v;  // Store the predecessor
+                  }
               }
           }
       }
   }
 
+  // Reconstruct the path using the prev table
   let mask = (1 << n) - 1;
   let u = 0;
+  let lastNode = -1;
   let minCost = Infinity;
   for (let v = 1; v < n; v++) {
       if (dp[mask][v] + dists[v][u] < minCost) {
           minCost = dp[mask][v] + dists[v][u];
+          lastNode = v;
       }
   }
-  return [minCost, dp];
-}
 
-function backtrack(dists: number[][], dp: number[][]): number[] {
-  const n: number = dists.length;
-  let mask: number = (1 << n) - 1;
-  let u: number = 0;
-  let prev: number = 0;
-  let tour: number[] = [0];
+  let path: number[] = [];
+  path.push(0);
+  path.push(lastNode);
+  mask ^= (1 << lastNode);
+  u = lastNode;
 
-  for (let _ = 1; _ < n; _++) {
-      let minDist: number = Infinity;
-      for (let v = 1; v < n; v++) {
-          if (mask & (1 << v) && dp[mask][v] + dists[v][u] < minDist) {
-              minDist = dp[mask][v] + dists[v][u];
-              prev = v;
-          }
-      }
-      tour.push(prev);
-      mask ^= (1 << prev);
-      u = prev;
+  while (u != 0) {
+      const v = prev[mask][u];
+      path.push(v);
+      mask ^= (1 << u);
+      u = v;
   }
-  tour.push(0);  // Return to the starting point.
-  return tour;
-}
 
+  path.reverse();  // Reverse the path to get the correct order
+  path.push(0);  // Return to the starting point
+
+  return [minCost, path];
+}
