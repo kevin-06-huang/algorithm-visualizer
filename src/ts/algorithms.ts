72e1371022696222a323103df3474bc92fa03762
Kevin <kevin.06.huang@gmail.com>
2023-08-24 22:39:22 -0700

    Add more comments

--- src/ts/algorithms.ts
+++ src/ts/algorithms.ts
@@ -1,20 +1,29 @@
 import { Point } from "../../types";
 
-// Function to compute the Euclidean distance between two points
+// Compute the Euclidean distance between two points
 function distance(p1: Point, p2: Point): number {
   return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
 }
 
 function heldKarp(dists: number[][]): [number, number[][]] {
   const n: number = dists.length;
-  const dp: number[][] = Array.from(Array(1 << n), () =>
+
+  // Initialize the DP table with all values set to Infinity
+  // Each row in the DP table corresponds to a subset of cities (represented in binary)
+  const dp: number[][] = Array.from({ length: 1 << n }, () =>
     Array(n).fill(Infinity),
   );
+
+  // Start with just the first city (0) visited
   dp[1][0] = 0;
 
+  // Loop over all subsets of cities
   for (let mask = 1; mask < 1 << n; mask++) {
     for (let u = 0; u < n; u++) {
       if (!(mask & (1 << u))) continue;
+
+      // Try to find the shortest path to city `u` 
+      // by considering all possible previous cities `v`
       for (let v = 0; v < n; v++) {
         if (mask & (1 << v) && u != v) {
           dp[mask][u] = Math.min(
@@ -26,6 +35,7 @@ function heldKarp(dists: number[][]): [number, number[][]] {
     }
   }
 
+  // Calculate the shortest cycle by returning to the starting city from every other city
   let mask = (1 << n) - 1;
   let u = 0;
   let minCost = Infinity;
@@ -37,6 +47,7 @@ function heldKarp(dists: number[][]): [number, number[][]] {
   return [minCost, dp];
 }
 
+// Reconstruct the optimal tour using the DP table
 function backtrack(dists: number[][], dp: number[][]): number[] {
   const n: number = dists.length;
   let mask: number = (1 << n) - 1;
@@ -44,6 +55,7 @@ function backtrack(dists: number[][], dp: number[][]): number[] {
   let prev: number = 0;
   let tour: number[] = [0];
 
+  // Reconstruct the path by backtracking from the last city
   for (let _ = 1; _ < n; _++) {
     let minDist: number = Infinity;
     for (let v = 1; v < n; v++) {
@@ -56,7 +68,9 @@ function backtrack(dists: number[][], dp: number[][]): number[] {
     mask ^= 1 << prev;
     u = prev;
   }
-  tour.push(0); // Return to the starting point.
+
+  // Append the starting city to form a complete cycle
+  tour.push(0);
   return tour;
 }
 
