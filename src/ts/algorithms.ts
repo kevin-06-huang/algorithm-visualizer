020304b687f7eaec3a3f1067094841c720d5c649
Kevin <kevin.06.huang@gmail.com>
2023-08-24 22:01:22 -0700

    Add heldKarp

--- src/ts/algorithms.ts
+++ src/ts/algorithms.ts
@@ -1,75 +1,63 @@
-// This function calculates the Euclidean distance between two points.
-function distance(p1: [number, number], p2: [number, number]): number {
-  return Math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2);
+import { Point } from "../../types";
+
+// Function to compute the Euclidean distance between two points
+function distance(p1: Point, p2: Point): number {
+  return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
 }
 
-function heldKarp(dists: number[][]): [number, number[]] {
-  // Number of cities or nodes.
+function heldKarp(dists: number[][]): [number, number[][]] {
   const n: number = dists.length;
-
-  // dp table will store the minimum cost of visiting a subset of cities.
   const dp: number[][] = Array.from(Array(1 << n), () =>
     Array(n).fill(Infinity),
   );
-
-  // prev table will store predecessors to help reconstruct the path later.
-  const prev: number[][] = Array.from(Array(1 << n), () => Array(n).fill(-1));
-
-  // Base case: The cost of visiting just the first city (0) is 0.
   dp[1][0] = 0;
 
-  // Iterate over all subsets of cities.
   for (let mask = 1; mask < 1 << n; mask++) {
     for (let u = 0; u < n; u++) {
-      // If city u is not in the current subset, continue.
       if (!(mask & (1 << u))) continue;
-
       for (let v = 0; v < n; v++) {
-        // If city v is in the subset and u is not equal to v.
         if (mask & (1 << v) && u != v) {
-          // Update the dp table if we find a shorter path.
-          if (dp[mask][u] > dp[mask ^ (1 << u)][v] + dists[v][u]) {
-            dp[mask][u] = dp[mask ^ (1 << u)][v] + dists[v][u];
-            prev[mask][u] = v; // Store the city v which led to the shorter path.
-          }
+          dp[mask][u] = Math.min(
+            dp[mask][u],
+            dp[mask ^ (1 << u)][v] + dists[v][u],
+          );
         }
       }
     }
   }
 
-  // Start reconstructing the path from the end.
-  let mask = (1 << n) - 1; // Represents all cities being visited.
-  let u = 0; // Start city.
-  let lastNode = -1;
+  let mask = (1 << n) - 1;
+  let u = 0;
   let minCost = Infinity;
-
-  // Find the city v that gives the minimum cost when ending the path at city v.
   for (let v = 1; v < n; v++) {
     if (dp[mask][v] + dists[v][u] < minCost) {
       minCost = dp[mask][v] + dists[v][u];
-      lastNode = v;
     }
   }
-
-  let path: number[] = [];
-  path.push(0);
-  path.push(lastNode);
-  mask ^= 1 << lastNode; // Remove city lastNode from the mask.
-  u = lastNode;
-
-  // Backtrack the path using the prev table.
-  while (u != 0) {
-    const v = prev[mask][u];
-    path.push(v);
-    mask ^= 1 << u; // Remove city u from the mask.
-    u = v;
-  }
-
-  // The path is built in reverse order, so we need to reverse it.
-  path.reverse();
-
-  // Append the start city to complete the cycle.
-  path.push(0);
-
-  return [minCost, path]; // Return the minimum cost and the corresponding path.
+  return [minCost, dp];
 }
+
+function backtrack(dists: number[][], dp: number[][]): number[] {
+  const n: number = dists.length;
+  let mask: number = (1 << n) - 1;
+  let u: number = 0;
+  let prev: number = 0;
+  let tour: number[] = [0];
+
+  for (let _ = 1; _ < n; _++) {
+    let minDist: number = Infinity;
+    for (let v = 1; v < n; v++) {
+      if (mask & (1 << v) && dp[mask][v] + dists[v][u] < minDist) {
+        minDist = dp[mask][v] + dists[v][u];
+        prev = v;
+      }
+    }
+    tour.push(prev);
+    mask ^= 1 << prev;
+    u = prev;
+  }
+  tour.push(0); // Return to the starting point.
+  return tour;
+}
+
+export { distance, heldKarp, backtrack };
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,6 +2,7 @@ import * as d3 from "d3";
 
 import data from "./data";
 import { drawPoints, initialize, width, height } from "./visualization";
+import { distance, heldKarp, backtrack } from "./algorithms";
 
 const svg = initialize();
 
@@ -14,5 +15,15 @@ svg.on("click", function (event) {
   const scaledY: number = -(y - height / 2) / 20;
 
   data.push({ x: scaledX, y: scaledY });
+
+  const dists: number[][] = Array.from({ length: data.length }, (_, i) =>
+    Array.from({ length: data.length }, (_, j) => distance(data[i], data[j])),
+  );
+
+  const [minDistance, dp] = heldKarp(dists);
+  const route: number[] = backtrack(dists, dp);
+  // console.log("Minimum Distance:", minDistance);
+  // console.log("Route:", route.map(idx => data[idx]));
+
   drawPoints(svg);
 });
